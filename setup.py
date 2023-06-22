d1d4da1b964aeed133155c1840c06f4633809947
Solmaz Khajehpour <solmazkh114@gmailcom>
2023-06-22 14:47:42 +0330

    version modified after adding multi-class classifcation

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='performance',
-    version='0.0.1',
+    version='0.0.2',
     description='An easy way of extracting all performance metrics of an ML model',
     py_modules=["ml-performance"],
     classifiers=[
